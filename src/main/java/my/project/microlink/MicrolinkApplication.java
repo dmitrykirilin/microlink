091ae3b79966a7d3d8c643452b9ee3107d29d62d
dmitrykirilin <demonus18@gmail.com>
2020-08-20 18:13:03 +0300

    Init project.

---
+++ src/main/java/my/project/microlink/MicrolinkApplication.java
@@ -0,0 +1,13 @@
+package my.project.microlink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicrolinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicrolinkApplication.class, args);
+	}
+
+}
